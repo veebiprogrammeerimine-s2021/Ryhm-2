7ba96e7286966beff910e6baf8f521c0054876e5
Andrus Rinde <andrusrinde@users.noreply.github.com>
2021-09-13 15:25:32 +0300

    Kaks esimest tundi

---
+++ photos/kolmnurk.py
@@ -0,0 +1,6 @@
+#Täisnurkse kolmnurga kohta on teada tema kaatetite pikkused.
+#Leida kolmnurga pindala.
+a = int(input("Esimene kaatet "))
+b = int(input("Teine kaatet "))
+s = a * b / 2
+print("Kolmnurga pindala on", S)
