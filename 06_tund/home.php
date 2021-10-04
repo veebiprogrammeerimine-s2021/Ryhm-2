ea867132acf4649135664a7ec33ce288a5294512
Andrus Rinde <andrusrinde@users.noreply.github.com>
2021-10-04 14:03:11 +0300

    Viies kodune töö tehtud

---
+++ 06_tund/home.php
@@ -0,0 +1,32 @@
+<?php
+    //alustame sessiooni
+    session_start();
+    //kas on sisselogitud
+    if(!isset($_SESSION["user_id"])){
+        header("Location: page.php");
+    }
+    //väljalogimine
+    if(isset($_GET["logout"])){
+        session_destroy();
+        header("Location: page.php");
+    }
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+	<meta charset="utf-8">
+	<title><?php echo $_SESSION["first_name"] ." " .$_SESSION["last_name"]; ?>, veebiprogrammeerimine</title>
+</head>
+<body>
+	<h1><?php echo $_SESSION["first_name"] ." " .$_SESSION["last_name"]; ?>, veebiprogrammeerimine</h1>
+	<p>See leht on valminud õppetöö raames ja ei sisalda mingisugust tõsiseltvõetavat sisu!</p>
+	<p>Õppetöö toimus <a href="https://www.tlu.ee/dt">Tallinna Ülikooli Digitehnoloogiate instituudis</a>.</p>
+	<hr>
+    <ul>
+        <li><a href="?logout=1">Logi välja</a></li>
+		<li><a href="list_films.php">Filmide nimekirja vaatamine</a> versioon 1</li>
+		<li><a href="add_films.php">Filmide lisamine andmebaasi</a> versioon 1</li>
+    </ul>
+    
+</body>
+</html>
